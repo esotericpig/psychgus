a3ec42aa1eca96d50a7c6c74ce96e3428d384097
esotericpig <esotericpig@users.noreply.github.com>
2019-06-18 00:44:55 +0800

    Add parse*(), dump*(), and helper methods

--- lib/psychgus.rb
+++ lib/psychgus.rb
@@ -24,10 +24,143 @@ require 'bundler/setup'
 require 'psych'
 
 require 'psychgus/blueberry'
+require 'psychgus/styled_document_stream'
 require 'psychgus/styled_tree_builder'
 require 'psychgus/styler'
 require 'psychgus/super_sniffer'
 require 'psychgus/version'
 
+class Object
+  # Don't use keyword args for options so can be a drop-in-replacement for Psych
+  def to_yaml(options={},**kargs)
+    return Psychgus.dump(self,options,**kargs)
+  end
+end
+
+module Psych
+  module Nodes
+    class Node
+      # Old versions of Psych didn't have alias?(), mapping?(), etc.
+      # name can be a symbol or a string
+      def node_of?(name)
+        return is_a?(Psychgus.node_class(name))
+      end
+    end
+  end
+end
+
 module Psychgus
+  def self.node_class(name)
+    name = name.to_sym().capitalize()
+    
+    case name
+    when :Doc
+      name = :Document
+    when :Map
+      name = :Mapping
+    when :Seq
+      name = :Sequence
+    end
+    
+    return Psych::Nodes.const_get(name)
+  end
+  
+  def self.node_const(class_name,const_name,lenient=true)
+    node_class = node_class(class_name)
+    const_name = const_name.upcase()
+    
+    return 0 if lenient && !node_class.const_defined?(const_name,true)
+    return node_class.const_get(const_name,true)
+  end
+  
+  MAPPING_ANY = node_const(:mapping,:any)
+  MAPPING_BLOCK = node_const(:mapping,:block)
+  MAPPING_FLOW = node_const(:mapping,:flow)
+  MAP_ANY = MAPPING_ANY
+  MAP_BLOCK = MAPPING_BLOCK
+  MAP_FLOW = MAPPING_FLOW
+  
+  SCALAR_ANY = node_const(:scalar,:any)
+  SCALAR_PLAIN = node_const(:scalar,:plain)
+  SCALAR_SINGLE_QUOTED = node_const(:scalar,:single_quoted)
+  SCALAR_DOUBLE_QUOTED = node_const(:scalar,:double_quoted)
+  SCALAR_LITERAL = node_const(:scalar,:literal)
+  SCALAR_FOLDED = node_const(:scalar,:folded)
+  
+  SEQUENCE_ANY = node_const(:sequence,:any)
+  SEQUENCE_BLOCK = node_const(:sequence,:block)
+  SEQUENCE_FLOW = node_const(:sequence,:flow)
+  SEQ_ANY = SEQUENCE_ANY
+  SEQ_BLOCK = SEQUENCE_BLOCK
+  SEQ_FLOW = SEQUENCE_FLOW
+  
+  STREAM_ANY = node_const(:stream,:any)
+  STREAM_UTF8 = node_const(:stream,:utf8)
+  STREAM_UTF16LE = node_const(:stream,:utf16le)
+  STREAM_UTF16BE = node_const(:stream,:utf16be)
+  
+  # You don't need to pass in stylers, if you're just going to call to_ruby()
+  def self.parse(yaml,**kargs)
+    parse_stream(yaml,**kargs) do |node|
+      return node
+    end
+    
+    return false
+  end
+  
+  # You don't need to pass in stylers, if you're just going to call to_ruby()
+  def self.parse_file(filename,fallback: false,**kargs)
+    result = File.open(filename,'r:BOM|UTF-8') do |file|
+      parse(file,filename: filename,**kargs)
+    end
+    
+    return result || fallback
+  end
+  
+  # You don't need to pass in stylers, if you're just going to call to_ruby()
+  def self.parse_stream(yaml,filename: nil,stylers: nil,&block)
+    if block_given?()
+      parser = Psych::Parser.new(StyledDocumentStream.new(*stylers,&block))
+      
+      return parser.parse(yaml,filename)
+    else
+      parser = self.parser(stylers: stylers)
+      parser.parse(yaml,filename)
+      
+      return parser.handler.root
+    end
+  end
+  
+  def self.parser(stylers: nil)
+    return Psych::Parser.new(StyledTreeBuilder.new(*stylers))
+  end
+  
+  # Don't use keyword args for io & options so can be a drop-in-replacement for Psych
+  def self.dump(object,io=nil,options={},**kargs)
+    return dump_stream(object,io: io,options: options,**kargs)
+  end
+  
+  # Mode can be 'w:UTF-8', 'a:UTF-16', etc.
+  # stylers: MyStyler.new
+  # stylers: [...]
+  def self.dump_file(filename,*objects,mode: 'w',perm: nil,opt: nil,**kargs)
+    File.open(filename,mode,perm,opt) do |file|
+      file.write(dump_stream(*objects,**kargs))
+    end
+  end
+  
+  def self.dump_stream(*objects,io: nil,options: {},stylers: nil)
+    if Hash === io
+      options = io
+      io = nil
+    end
+    
+    visitor = Psych::Visitors::YAMLTree.create(options,StyledTreeBuilder.new(*stylers))
+    
+    objects.each do |object|
+      visitor << object
+    end
+    
+    return visitor.tree.yaml(io,options)
+  end
 end
