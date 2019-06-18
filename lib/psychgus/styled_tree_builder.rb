6b2ffd6b72fdae959595d0a4df31f6b9ef27682c
esotericpig <esotericpig@users.noreply.github.com>
2019-06-18 23:56:08 +0800

    Add documentation

--- lib/psychgus/styled_tree_builder.rb
+++ lib/psychgus/styled_tree_builder.rb
@@ -25,10 +25,26 @@ require 'psychgus/styler'
 require 'psychgus/super_sniffer'
 
 module Psychgus
+  ###
+  # Use this wherever Psych::TreeBuilder would have been used, to enable styling.
+  # 
+  # @author Jonathan Bradley Whited (@esotericpig)
+  # @since  1.0.0
+  # 
+  # @see Psychgus.parser Psychgus.parser
+  # @see Psychgus.dump_stream Psychgus.dump_stream
+  # @see Psych::TreeBuilder
+  ###
   class StyledTreeBuilder < Psych::TreeBuilder
+    # @return [SuperSniffer] the {SuperSniffer} being used to sniff the YAML nodes, level, etc.
     attr_reader :sniffer
+    
+    # @return [Array<Stylers>] the {Styler}(s) being used to style the YAML nodes
     attr_reader :stylers
     
+    # Initialize this class with {Styler}(s).
+    # 
+    # @param styler [Styler] {Styler}(s) to use for styling this TreeBuilder
     def initialize(*styler)
       super()
       
@@ -38,12 +54,25 @@ module Psychgus
       add_styler(*styler)
     end
     
+    # Add {Styler}(s) onto the end of the data structure.
+    # 
+    # @param styler [Styler] {Styler}(s) to add
+    # 
+    # @return [self] this class
     def add_styler(*styler)
       @stylers.push(*styler)
       
       return self
     end
     
+    # Calls super, styler(s), and sniffer.
+    # 
+    # @see Psych::TreeBuilder#alias
+    # @see Styler#style
+    # @see Styler#style_alias
+    # @see SuperSniffer#add_alias
+    # 
+    # @return [Psych::Nodes::Alias] the alias node created
     def alias(*)
       node = super
       
@@ -57,34 +86,77 @@ module Psychgus
       return node
     end
     
+    # Calls super and sniffer.
+    # 
+    # @see Psych::TreeBuilder#end_mapping
+    # @see SuperSniffer#end_mapping
     def end_mapping(*)
-      super
+      result = super
+      
       @sniffer.end_mapping()
+      
+      return result
     end
     
+    # Calls super and sniffer.
+    # 
+    # @see Psych::TreeBuilder#end_sequence
+    # @see SuperSniffer#end_sequence
     def end_sequence(*)
-      super
+      result = super
+      
       @sniffer.end_sequence()
+      
+      return result
     end
     
+    # Insert {Styler}(s) at +index+ into the data structure.
+    # 
+    # @param styler [Styler] {Styler}(s) to insert
+    # 
+    # @return [self] this class
     def insert_styler(index,*styler)
       @stylers.insert(index,*styler)
       
       return self
     end
     
+    # Remove the last {Styler} from the data structure.
+    # 
+    # @return [Styler,nil] the last {Styler} or nil if empty
     def pop_styler()
       return @stylers.pop()
     end
     
+    # Remove the {Styler} that matches +styler+ from the data structure.
+    # 
+    # An optional +block+ can return a default value if not found.
+    # 
+    # @param styler [Styler] the {Styler} to find and remove
+    # @param block [Proc] an optional block to call when +styler+ is not found
+    # 
+    # @return [Styler,nil] the last {Styler}, or if not found, nil or the result of +block+
     def remove_styler(styler,&block)
       return @stylers.delete(styler,&block)
     end
     
+    # Remove the {Styler} at +index+ from the data structure.
+    # 
+    # @param index [Integer] the index of the {Styler} to remove
+    # 
+    # @return [Styler,nil] the {Styler} removed or nil if empty
     def remove_styler_at(index)
       return @stylers.delete_at(index)
     end
     
+    # Calls super, styler(s), and sniffer.
+    # 
+    # @see Psych::TreeBuilder#scalar
+    # @see Styler#style
+    # @see Styler#style_scalar
+    # @see SuperSniffer#add_scalar
+    # 
+    # @return [Psych::Nodes::Scalar] the scalar node created
     def scalar(*)
       node = super
       
@@ -98,6 +170,14 @@ module Psychgus
       return node
     end
     
+    # Calls super, styler(s), and sniffer.
+    # 
+    # @see Psych::TreeBuilder#start_mapping
+    # @see Styler#style
+    # @see Styler#style_mapping
+    # @see SuperSniffer#start_mapping
+    # 
+    # @return [Psych::Nodes::Mapping] the mapping node created
     def start_mapping(*)
       node = super
       
@@ -111,6 +191,14 @@ module Psychgus
       return node
     end
     
+    # Calls super, styler(s), and sniffer.
+    # 
+    # @see Psych::TreeBuilder#start_sequence
+    # @see Styler#style
+    # @see Styler#style_sequence
+    # @see SuperSniffer#start_sequence
+    # 
+    # @return [Psych::Nodes::Sequence] the sequence node created
     def start_sequence(*)
       node = super
       
