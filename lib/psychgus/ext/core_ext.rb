23367457dcd710a68dcb1783f4b60206bf8f2539
esotericpig <esotericpig@users.noreply.github.com>
2019-06-18 15:15:11 +0800

    Initial commit

---
+++ lib/psychgus/ext/core_ext.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+###
+# This file is part of Psychgus.
+# Copyright (c) 2019 Jonathan Bradley Whited (@esotericpig)
+# 
+# Psychgus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Psychgus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with Psychgus.  If not, see <http://www.gnu.org/licenses/>.
+###
+
+class Object
+  # Don't use keyword args for options so can be a drop-in-replacement for Psych
+  def to_yaml(options={},**kargs)
+    return Psychgus.dump(self,options,**kargs)
+  end
+end
---
+++ lib/psychgus/ext/node_ext.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+###
+# This file is part of Psychgus.
+# Copyright (c) 2019 Jonathan Bradley Whited (@esotericpig)
+# 
+# Psychgus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Psychgus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with Psychgus.  If not, see <http://www.gnu.org/licenses/>.
+###
+
+module Psych
+  module Nodes
+    class Node
+      # Old versions of Psych didn't have alias?(), mapping?(), etc.
+      # name can be a symbol or a string
+      def node_of?(name)
+        return is_a?(Psychgus.node_class(name))
+      end
+    end
+  end
+end
