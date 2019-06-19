5051e3a71226a0132d9f4f64dc5a46528bc766b3
esotericpig <esotericpig@users.noreply.github.com>
2019-06-19 13:50:27 +0800

    Add documentation

--- lib/psychgus/styler.rb
+++ lib/psychgus/styler.rb
@@ -28,13 +28,81 @@ module Psychgus
     end
   end
   
+  ###
+  # Mix in (include) this module to make a class/module/etc. a styler for YAML.
+  # 
+  # Although it's unnecessary (because of Duck Typing), it's the recommended practice in case a new method is
+  # added in the future, and also so you don't have to define methods that you don't use.
+  # 
+  # You can either use this as is (see example) or inside of a class (see {Blueberry}).
+  # 
+  # @example
+  #   class MyStyler
+  #     include Psychgus::Styler
+  #     
+  #     def style_sequence(sniffer,node)
+  #       node.style = Psychgus::SEQUENCE_FLOW if sniffer.level == 3
+  #     end
+  #   end
+  #   
+  #   hash = {'Coffee'=>{
+  #             'Roast'=>['Light','Medium','Dark','Extra Dark'],
+  #             'Style'=>['Cappuccino','Espresso','Latte','Mocha']
+  #          }}
+  #   puts hash.to_yaml(stylers: MyStyler.new())
+  #   
+  #   # Output:
+  #   # ---
+  #   # Coffee:
+  #   #   Roast: [Light, Medium, Dark, Extra Dark]
+  #   #   Style: [Cappuccino, Espresso, Latte, Mocha]
+  # 
+  # @author Jonathan Bradley Whited (@esotericpig)
+  # @since  1.0.0
+  # 
+  # @see Psychgus
+  # @see Object#to_yaml
+  # @see Blueberry
+  # @see StyledTreeBuilder#initialize
+  # @see StyledDocumentStream#initialize
+  # @see YAMLTreeExt#accept
+  ###
   module Styler
     EMPTY = Empty.new().freeze()
     
+    # Style a node of any type.
+    # 
+    # You can use {Psych::Nodes::Node#node_of?} to determine its type:
+    #   puts node.value if node.node_of?(:scalar)
+    # 
+    # @param sniffer [SuperSniffer] passed in from {StyledTreeBuilder}
+    # @param node [Psych::Nodes::Node] passed in from {StyledTreeBuilder}
+    # 
+    # @see Psych::Nodes::Node#node_of?
     def style(sniffer,node) end
+    
+    # Style a node guaranteed to be of type Psych::Nodes::Alias, to avoid if statements.
+    # 
+    # @param sniffer [SuperSniffer] passed in from {StyledTreeBuilder}
+    # @param node [Psych::Nodes::Alias] of type Alias passed in from {StyledTreeBuilder}
     def style_alias(sniffer,node) end
+    
+    # Style a node guaranteed to be of type Psych::Nodes::Mapping, to avoid if statements.
+    # 
+    # @param sniffer [SuperSniffer] passed in from {StyledTreeBuilder}
+    # @param node [Psych::Nodes::Mapping] of type Mapping passed in from {StyledTreeBuilder}
     def style_mapping(sniffer,node) end
+    
+    # Style a node guaranteed to be of type Psych::Nodes::Scalar, to avoid if statements.
+    # 
+    # @param sniffer [SuperSniffer] passed in from {StyledTreeBuilder}
+    # @param node [Psych::Nodes::Scalar] of type Scalar passed in from {StyledTreeBuilder}
     def style_scalar(sniffer,node) end
+    
+    # Style a node guaranteed to be of type Psych::Nodes::Sequence, to avoid if statements.
+    # 
+    # @param sniffer [SuperSniffer] passed in from {StyledTreeBuilder}
+    # @param node [Psych::Nodes::Sequence] of type Sequence passed in from {StyledTreeBuilder}
     def style_sequence(sniffer,node) end
   end
 end
